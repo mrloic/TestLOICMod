d4dbe6734773a185ebfa73c408ec421f7470b272
Kirill Safin <safinkirill2003@mail.ru>
2024-01-21 02:09:47 +0500

    p

--- src/main/java/net/mrloic/loicmod/utils/ModItemGroups.java
+++ src/main/java/net/mrloic/loicmod/utils/ModItemGroups.java
@@ -1,4 +1,22 @@
 package net.mrloic.loicmod.utils;
 
-public class ModItemGroups {
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.ItemLike;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModItemGroups
+{
+    private static CreativeModeTab createTab(String label, RegistryObject iconItem)
+    {
+        return new CreativeModeTab(label)
+        {
+            @Override
+            public ItemStack makeIcon()
+            {
+                return new ItemStack((ItemLike) iconItem.get());
+            }
+        };
+    }
+    public static final CreativeModeTab LOIC_ITEMS_TAB = createTab("loic_items_tab", ModItems.LOIC);
 }
