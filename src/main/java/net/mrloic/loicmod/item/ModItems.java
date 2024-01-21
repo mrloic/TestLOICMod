5450ff7b083133e6f6b241b0147cc1bbbe65fe52
klim0 <klim0909@yandex.ru>
2024-01-21 23:42:12 +0500

    t

--- src/main/java/net/mrloic/loicmod/item/ModItems.java
+++ src/main/java/net/mrloic/loicmod/item/ModItems.java
@@ -1,6 +1,6 @@
 package net.mrloic.loicmod.item;
 
-import net.minecraft.world.item.CreativeModeTab;
+
 import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
